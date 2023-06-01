68bd0c015509bc87f600275f9b8109c6c8b914f4
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2023-06-01 14:44:19 +0900

    add Singleton code

---
+++ Creational/Singleton/src/SingletonThreadSafe.java
@@ -0,0 +1,22 @@
+public final class SingletonThreadSafe {
+    private static volatile SingletonThreadSafe instance;
+    public String value;
+
+    private SingletonThreadSafe(String value) {
+        this.value = value;
+    }
+
+    public static SingletonThreadSafe getInstance(String value) {
+        SingletonThreadSafe result = instance;
+        if (result != null) {
+            return result;
+        }
+
+        synchronized (SingletonThreadSafe.class) {
+            if (instance == null) {
+                instance = new SingletonThreadSafe(value);
+            }
+            return instance;
+        }
+    }
+}
